a1c4fb1cc0d3daaf2e1e138c4a2f2607fdf1a522
mijin0721 <kgmi2010@naver.com>
2024-01-20 00:35:13 +0900

    ListView

--- app/src/main/java/com/example/android_study/MainActivity.java
+++ app/src/main/java/com/example/android_study/MainActivity.java
@@ -7,43 +7,41 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.sax.StartElementListener;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btn_move;
-    private  EditText et_test;
-    private String str;
-    private ImageView img_test;
+    private ListView list_test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_move = findViewById(R.id.btn_move);
-        et_test = findViewById(R.id.et_test);
-        img_test = findViewById(R.id.img_test);
+        list_test = (ListView) findViewById(R.id.List_test);
 
-        btn_move.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                str = et_test.getText().toString();
-                Intent intent = new Intent(MainActivity.this, SubActivity.class);
-                intent.putExtra("str", str);
-                startActivity(intent);  //액티비티 이동
+        List<String> data = new ArrayList<String>() {
+            {
+                add("홍길동");
+                add("콩쥐");
+                add("팥쥐");
             }
-        });
+        };
 
-        img_test.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "안녕하세요!", Toast.LENGTH_SHORT).show();
-            }
-        });
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, data);
+
+        list_test.setAdapter(adapter);
+
+        adapter.notifyDataSetChanged();
 
     }
 }
