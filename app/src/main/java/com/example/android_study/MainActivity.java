1df6951c4962f1383cb95ff5dd4805c7c3d14212
mijin0721 <kgmi2010@naver.com>
2024-01-19 23:45:29 +0900

    ImageView / Toast

--- app/src/main/java/com/example/android_study/MainActivity.java
+++ app/src/main/java/com/example/android_study/MainActivity.java
@@ -9,12 +9,15 @@ import android.sax.StartElementListener;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btn_move;
     private  EditText et_test;
     private String str;
+    private ImageView img_test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
         btn_move = findViewById(R.id.btn_move);
         et_test = findViewById(R.id.et_test);
+        img_test = findViewById(R.id.img_test);
 
         btn_move.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,6 +35,13 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(MainActivity.this, SubActivity.class);
                 intent.putExtra("str", str);
                 startActivity(intent);  //액티비티 이동
+            }
+        });
+
+        img_test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "안녕하세요!", Toast.LENGTH_SHORT).show();
             }
         });
 
