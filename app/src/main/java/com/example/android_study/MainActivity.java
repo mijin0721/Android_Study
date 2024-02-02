f1e107e840d5cee7312d46f00ea8b772e7466537
mijin0721 <kgmi2010@naver.com>
2024-02-03 00:19:40 +0900

    log 및 주석 달기

--- app/src/main/java/com/example/android_study/MainActivity.java
+++ app/src/main/java/com/example/android_study/MainActivity.java
@@ -4,61 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button btn1, btn2, btn3, btn4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn1 = (Button) findViewById(R.id.btn_1);
-        btn2 = (Button) findViewById(R.id.btn_2);
-        btn3 = (Button) findViewById(R.id.btn_3);
-        btn4 = (Button) findViewById(R.id.btn_4);
+        String hi = "안녕";
 
-        btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                Fragment1 fragment1 = new Fragment1();
-                transaction.replace(R.id.frame, fragment1);
-                transaction.commit();
-            }
-        });
+        Log.e("MainActivity : ", hi);
 
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                Fragment2 fragment2 = new Fragment2();
-                transaction.replace(R.id.frame, fragment2);
-                transaction.commit();
-            }
-        });
+        int a = 10; // 안녕의 나이
 
-        btn3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                Fragment3 fragment3 = new Fragment3();
-                transaction.replace(R.id.frame, fragment3);
-                transaction.commit();
-            }
-        });
-
-        btn4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                Fragment4 fragment4 = new Fragment4();
-                transaction.replace(R.id.frame, fragment4);
-                transaction.commit();
-            }
-        });
+        Log.e("MainActivity : ", String.valueOf(a));
     }
 }
