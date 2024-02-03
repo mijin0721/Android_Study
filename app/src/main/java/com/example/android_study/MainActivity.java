823786927bb03571120782bbaacc1d8e0493b34d
mijin0721 <kgmi2010@naver.com>
2024-02-04 00:07:23 +0900

    Thread & Handler

--- app/src/main/java/com/example/android_study/MainActivity.java
+++ app/src/main/java/com/example/android_study/MainActivity.java
@@ -1,26 +1,64 @@
 package com.example.android_study;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_start, btn_stop;
+    Thread thread;
+    boolean isThread = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String hi = "안녕";
+        btn_start = (Button) findViewById(R.id.btn_start);
+        btn_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                isThread = true;
+                thread = new Thread() {
+                    public void run() {
+                        while (isThread) {
+                            try {
+                                sleep(5000);
+                            } catch (InterruptedException e) {
+                                throw new RuntimeException(e);
+                            }
+                            handler.sendEmptyMessage(0);
+                        }
+                    }
+                };
+                thread.start();
+            }
+        });
 
-        Log.e("MainActivity : ", hi);
+        btn_stop = (Button) findViewById(R.id.btn_stop);
+        btn_stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                isThread = false;
+            }
+        });
 
-        int a = 10; // 안녕의 나이
 
-        Log.e("MainActivity : ", String.valueOf(a));
     }
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            Toast.makeText(getApplicationContext(), "안녕", Toast.LENGTH_SHORT).show();
+        }
+    };
 }
