5b7bd58b06f6959c07e392c4886fd52a5fdc5a30
mijin0721 <kgmi2010@naver.com>
2024-02-04 22:16:44 +0900

    Dialog

--- app/src/main/java/com/example/android_study/MainActivity.java
+++ app/src/main/java/com/example/android_study/MainActivity.java
@@ -1,64 +1,65 @@
 package com.example.android_study;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn_start, btn_stop;
-    Thread thread;
-    boolean isThread = false;
+    Button btn_dialog;
+    TextView tv_result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_start = (Button) findViewById(R.id.btn_start);
-        btn_start.setOnClickListener(new View.OnClickListener() {
+        btn_dialog = (Button) findViewById(R.id.btn_dialog);
+        tv_result = (TextView) findViewById(R.id.tv_result);
+
+        btn_dialog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                isThread = true;
-                thread = new Thread() {
-                    public void run() {
-                        while (isThread) {
-                            try {
-                                sleep(5000);
-                            } catch (InterruptedException e) {
-                                throw new RuntimeException(e);
-                            }
-                            handler.sendEmptyMessage(0);
-                        }
+                AlertDialog.Builder ad = new AlertDialog.Builder(MainActivity.this);
+                ad.setIcon(R.mipmap.ic_launcher);
+                ad.setTitle("제목");
+                ad.setMessage("안녕?");
+
+                final EditText et = new EditText(MainActivity.this);
+                ad.setView(et);
+
+                ad.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        String result = et.getText().toString();
+                        tv_result.setText(result);
+                        dialogInterface.dismiss();
                     }
-                };
-                thread.start();
+                });
+
+                ad.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+
+                ad.show();
             }
         });
-
-        btn_stop = (Button) findViewById(R.id.btn_stop);
-        btn_stop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                isThread = false;
-            }
-        });
-
 
     }
 
-    private Handler handler = new Handler() {
-        @Override
-        public void handleMessage(@NonNull Message msg) {
-            Toast.makeText(getApplicationContext(), "안녕", Toast.LENGTH_SHORT).show();
-        }
-    };
 }
