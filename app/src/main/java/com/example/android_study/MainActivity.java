e8473e54b15ba5d2e1cac4460fb8c39d4d00d48e
김미진 <kgmi2010@naver.com>
2024-02-24 23:44:13 +0900

    음악재생 MP3

--- app/src/main/java/com/example/android_study/MainActivity.java
+++ app/src/main/java/com/example/android_study/MainActivity.java
@@ -2,13 +2,53 @@ package com.example.android_study;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_play, btn_stop;
+
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_play = findViewById(R.id.btn_play);
+        btn_stop = findViewById(R.id.btn_stop);
+
+        //재생 버튼 눌렀을 때
+        btn_play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.music);
+                mediaPlayer.start();
+            }
+        });
+
+        // 정지 버튼 눌렀을 때
+        btn_stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mediaPlayer.isPlaying()) {
+                    mediaPlayer.stop();
+                    mediaPlayer.reset();
+                }
+            }
+        });
+    }
+
+    // 액티비가 종료될 때 이 메소드 실행됨
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
     }
 }
