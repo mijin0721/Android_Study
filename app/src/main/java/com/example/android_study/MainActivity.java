c51aeb1b0abeadc87de943974e17e990172076c2
김미진 <kgmi2010@naver.com>
2024-02-25 22:18:17 +0900

    뒤로가기 두번눌러 앱종료

--- app/src/main/java/com/example/android_study/MainActivity.java
+++ app/src/main/java/com/example/android_study/MainActivity.java
@@ -1,54 +1,31 @@
 package com.example.android_study;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.media.MediaPlayer;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn_play, btn_stop;
-
-    MediaPlayer mediaPlayer;
+    private long backBtnTime = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        btn_play = findViewById(R.id.btn_play);
-        btn_stop = findViewById(R.id.btn_stop);
-
-        //재생 버튼 눌렀을 때
-        btn_play.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.music);
-                mediaPlayer.start();
-            }
-        });
-
-        // 정지 버튼 눌렀을 때
-        btn_stop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (mediaPlayer.isPlaying()) {
-                    mediaPlayer.stop();
-                    mediaPlayer.reset();
-                }
-            }
-        });
     }
 
-    // 액티비가 종료될 때 이 메소드 실행됨
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if (mediaPlayer != null) {
-            mediaPlayer.release();
-            mediaPlayer = null;
+    public void onBackPressed() {
+        long crtme = System.currentTimeMillis();
+        long gapTme = crtme - backBtnTime;
+
+        if(0 <= gapTme && 2000 >= gapTme) {
+            super.onBackPressed();
+        } else {
+            backBtnTime = crtme;
+            Toast.makeText(this, "종료", Toast.LENGTH_SHORT).show();
         }
     }
 }
