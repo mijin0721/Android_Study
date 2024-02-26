bcb4431db11ce9f8eaa91cab23944078ca3c3ac0
김미진 <kgmi2010@naver.com>
2024-02-26 22:24:17 +0900

    구글맵

--- app/src/main/java/com/example/android_study/MainActivity.java
+++ app/src/main/java/com/example/android_study/MainActivity.java
@@ -1,31 +1,42 @@
 package com.example.android_study;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.widget.Toast;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import android.app.FragmentManager;
+import android.os.Bundle;
 
-    private long backBtnTime = 0;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private FragmentManager fragmentManager;
+    private MapFragment mapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragmentManager = getFragmentManager();
+        mapFragment = (MapFragment) fragmentManager.findFragmentById(R.id.google_map);
+        mapFragment.getMapAsync(this);
     }
 
     @Override
-    public void onBackPressed() {
-        long crtme = System.currentTimeMillis();
-        long gapTme = crtme - backBtnTime;
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        LatLng location = new LatLng(37.85922, 127.7278); // 황금돼지저금통
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.title("황금돼지저금통");
+        markerOptions.snippet("음식점");
+        googleMap.addMarker(markerOptions);
 
-        if(0 <= gapTme && 2000 >= gapTme) {
-            super.onBackPressed();
-        } else {
-            backBtnTime = crtme;
-            Toast.makeText(this, "종료", Toast.LENGTH_SHORT).show();
-        }
+//        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 16));
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location, 16));
     }
 }
