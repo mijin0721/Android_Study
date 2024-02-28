9bb4f262ae7671650d2c8d2295b208bd6aabb8dd
김미진 <kgmi2010@naver.com>
2024-02-28 22:22:51 +0900

    StartActivityForResult

--- app/src/main/java/com/example/android_study/MainActivity.java
+++ app/src/main/java/com/example/android_study/MainActivity.java
@@ -1,100 +1,52 @@
 package com.example.android_study;
 
-import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.MenuItem;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private BottomNavigationView bottomNavigationView;
-    private FragmentManager fm;
-    private FragmentTransaction ft;
-    private Frag1 frag1;
-    private Frag2 frag2;
-    private Frag3 frag3;
-    private Frag4 frag4;
-    private Frag5 frag5;
+    private TextView tv_comeback;
+    private Button btn_go;
+
+    private static final int REQUEST_CODE = 777;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        bottomNavigationView = findViewById(R.id.bottomNavi);
-        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+
+        tv_comeback = findViewById(R.id.tv_comeback);
+        btn_go = findViewById(R.id.btn_go);
+
+        btn_go.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getTitle().toString()) {
-                    case "grade" :
-                        setFrag(0);
-                        break;
-
-                    case "fire" :
-                        setFrag(1);
-                        break;
-
-                    case "phone" :
-                        setFrag(2);
-                        break;
-
-                    case "send" :
-                        setFrag(3);
-                        break;
-
-                    case "wc" :
-                        setFrag(4);
-                        break;
-                }
-                return true;
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SubActivity.class);
+                startActivityForResult(intent, REQUEST_CODE);
             }
         });
-        frag1 = new Frag1();
-        frag2 = new Frag2();
-        frag3 = new Frag3();
-        frag4 = new Frag4();
-        frag5 = new Frag5();
-        
-        setFrag(0);
     }
-    
-    // 프레그먼트 교체가 일어나는 실행문
-    private void setFrag(int n) {
-        fm = getSupportFragmentManager();
-        ft = fm.beginTransaction();
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
         
-        switch (n) {
-            case 0 :
-                ft.replace(R.id.main_frame, frag1);
-                ft.commit();
-                break;
+        if (resultCode == RESULT_OK) {
+            Toast.makeText(getApplicationContext(), "수신 성공", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "수신 실패", Toast.LENGTH_SHORT).show();
+        }
 
-            case 1 :
-                ft.replace(R.id.main_frame, frag2);
-                ft.commit();
-                break;
-
-            case 2 :
-                ft.replace(R.id.main_frame, frag3);
-                ft.commit();
-                break;
-
-            case 3 :
-                ft.replace(R.id.main_frame, frag4);
-                ft.commit();
-                break;
-
-            case 4 :
-                ft.replace(R.id.main_frame, frag5);
-                ft.commit();
-                break;
+        if (requestCode == REQUEST_CODE) {
+            String resultTxt = data.getStringExtra("comeback");
+            tv_comeback.setText(resultTxt);
         }
     }
 }
---
+++ app/src/main/java/com/example/android_study/SubActivity.java
@@ -0,0 +1,34 @@
+package com.example.android_study;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SubActivity extends AppCompatActivity {
+
+    private EditText et_comeback;
+    private Button btn_close;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+
+        et_comeback = (EditText) findViewById(R.id.et_comeback);
+        btn_close = (Button) findViewById(R.id.btn_close);
+
+        btn_close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.putExtra("comeback", et_comeback.getText().toString());
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+    }
+}
