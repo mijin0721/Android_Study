26ecfac23e8e983d2c3c0d2aad642bca997dbc90
김미진 <kgmi2010@naver.com>
2024-03-05 20:54:54 +0900

    Constraint Layout

--- app/src/main/java/com/example/android_study/MainActivity.java
+++ app/src/main/java/com/example/android_study/MainActivity.java
@@ -2,28 +2,13 @@ package com.example.android_study;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.net.Uri;
 import android.os.Bundle;
-import android.widget.MediaController;
-import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private VideoView videoView;
-    private MediaController mediaController;
-    private String videoURL = "https://www.pexels.com/ko-kr/video/1321208/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        videoView = (VideoView) findViewById(R.id.videoView);
-        mediaController = new MediaController(this);
-        mediaController.setAnchorView(videoView);
-        Uri uri = Uri.parse(videoURL);
-        videoView.setMediaController(mediaController);
-        videoView.setVideoURI(uri);
-        videoView.start();
     }
 }
