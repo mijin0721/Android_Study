0281ef4af0579e4012e9269c9ab58b3aa7f85c0a
김미진 <kgmi2010@naver.com>
2024-03-22 14:10:17 +0900

    Radio Button

--- app/src/main/java/com/example/android_study/MainActivity.java
+++ app/src/main/java/com/example/android_study/MainActivity.java
@@ -1,28 +1,51 @@
 package com.example.android_study;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
-import android.widget.TableLayout;
-
-import com.google.android.material.tabs.TabLayout;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private FragmentPagerAdapter fragmentPagerAdapter;
+    private RadioGroup rb_gender;
+    private RadioButton rb_man, rb_woman;
+    private Button btn_result;
+    private String str_result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ViewPager viewPager = findViewById(R.id.viewPager);
-        fragmentPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        rb_gender = findViewById(R.id.rg_gender);
+        rb_man = findViewById(R.id.rb_man);
+        rb_woman = findViewById(R.id.rb_woman);
+        btn_result = findViewById(R.id.btn_result);
 
-        TabLayout tabLayout = findViewById(R.id.tab_Layout);
-        viewPager.setAdapter(fragmentPagerAdapter);
-        tabLayout.setupWithViewPager(viewPager);
+        rb_gender.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                if (i == R.id.rb_man) {
+                    str_result = "man";
+                } else if (i == R.id.rb_woman) {
+                    str_result = "woman";
+                }
+            }
+        });
+
+        btn_result.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (str_result != null) {
+                    Toast.makeText(MainActivity.this, str_result, Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "선택요망", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
